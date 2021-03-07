357192579786264fefff73fc2624d142562a45a9
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-07 11:49:59 +0200

    Updates

---
+++ precache-manifest.8a12f3f3c73dc1feb752802720126690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b89697cf6ba12702ad2e9a2a87a8a6",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "95dc87d2ef41b6321bad",
+    "url": "/Covid-19/static/css/main.80f0e920.chunk.css"
+  },
+  {
+    "revision": "b9c8fb09ff82c698eaed",
+    "url": "/Covid-19/static/js/2.d23b7b06.chunk.js"
+  },
+  {
+    "revision": "95dc87d2ef41b6321bad",
+    "url": "/Covid-19/static/js/main.81599e1c.chunk.js"
+  },
+  {
+    "revision": "4b5d3309fb2bc4984b16",
+    "url": "/Covid-19/static/js/runtime-main.f03a1828.js"
+  },
+  {
+    "revision": "061632d5406dc6cc81e9190f8de4a4e2",
+    "url": "/Covid-19/static/media/Prevent.061632d5.png"
+  }
+]);
